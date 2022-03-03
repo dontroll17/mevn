49ab29728f4d6d5ea9f5054c24d21f4ede049d77
dontroll17 <dontroll@yandex.ru>
2022-03-03 19:43:29 +0300

    get and put methods for 1 post by id

--- mevn-app/server/src/routes/posts.js
+++ mevn-app/server/src/routes/posts.js
@@ -26,6 +26,31 @@ router.get('/posts', (req, res) => {
     }).sort({ _id: -1 });
 });
 
+router.get('/posts/:id', (req, res) => {
+   Post.findById(req.params.id, 'title description', (err, post) => {
+       if(err) res.sendStatus(500);
+       else res.send(post);
+   });
+});
+
+router.put('/posts/:id', (req, res) => {
+    Post.findById(req.params.id, 'title description', (err, post) => {
+        if(err) console.error(err);
+        else {
+            if(req.body.title) {
+                post.title = req.body.title;
+            }
+            if(req.body.description) {
+                post.description = req.body.description;
+            }
+            post.save(e => {
+                if(e) res.sendStatus(500);
+                else res.sendStatus(200);
+            });
+        }
+    });
+});
+
 module.exports = router;
 
 
